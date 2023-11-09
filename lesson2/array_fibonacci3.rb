8ccb0d256be943473ec7c8e2b7bf4bff8996a520
sermick9 <148490202+sermick9@users.noreply.github.com>
2023-11-09 06:59:30 +0300

    Delete lesson2 directory

--- lesson2/array_fibonacci3.rb
+++
@@ -1,5 +0,0 @@
-fibonacci = [0, 1]
-while (next_fib = fibonacci[-1] + fibonacci[-2]) <= 100
-  fibonacci.append(next_fib)
-end
-puts fibonacci
--- lesson2/array_numbers2.rb
+++
@@ -1,2 +0,0 @@
-array = (10..100).step(5) {|array| puts array}
-
